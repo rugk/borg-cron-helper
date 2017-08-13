11f7ba6814d1080901a75984dac9f192a25bc742
rugk <rugk@posteo.de>
2017-08-13 15:51:28 +0200

    Add system scripts
    Including systemd stuff, logrotate and an init.d script.
    Fixes https://github.com/rugk/borg-cron-helper/issues/11
    Fixes https://github.com/rugk/borg-cron-helper/issues/10

---
+++ system/init.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+### BEGIN INIT INFO
+# Provides:          borg-cron-helper
+# Required-Start:    $local_fs
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Creates the RUNTIME dir at startup for the local lock system.
+### END INIT INFO
+
+# settings for directory
+RUN_PID_DIR="/var/run/borg"
+PERMISSIONS="0755"
+USER="backupuser"
+GROUP="backupuser"
+
+case "$1" in
+	start)
+		if [ ! -d "$RUN_PID_DIR" ]; then
+			mkdir -p "$RUN_PID_DIR"
+			chown "$USER":"$GROUP" "$RUN_PID_DIR"
+			chmod "$PERMISSIONS" "$RUN_PID_DIR"
+		fi
+	;;
+
+	stop)
+		[ -d "$RUN_PID_DIR" ] && rm -rf "$RUN_PID_DIR"
+	;;
+
+	*)
+		echo "Usage: $N {start|stop}" >&2
+		exit 1
+	;;
+esac
+
+exit 0
