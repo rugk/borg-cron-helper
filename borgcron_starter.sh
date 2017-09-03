05fc5f24840d29cc04b9963111156b0918987f76
rugk <rugk@posteo.de>
2017-09-03 13:41:13 +0200

    Fix indentation

--- borgcron_starter.sh
+++ borgcron_starter.sh
@@ -47,16 +47,16 @@ case "$1" in
 		cli_help
 		exit
 		;;
-        *) # specific config file(s) passed
-                for configfile in "$@"; do
-                        # remove possible ".sh" ending
-                        configfile="$( echo "$configfile"|sed 's/.sh$//')"
+		*) # specific config file(s) passed
+				for configfile in "$@"; do
+						# remove possible ".sh" ending
+						configfile="$( echo "$configfile"|sed 's/.sh$//')"
 
-                        if [ -e "$CONFIG_DIR/$configfile.sh" ]; then
-                                "$CURRENT_DIR/borgcron.sh" "$( get_full_path "$CONFIG_DIR/$configfile.sh" )"
-                        else
-                                echo "The backup settings file \"$configfile.sh\" could not be found." >&2
-                        fi
-                done
-                ;;
+						if [ -e "$CONFIG_DIR/$configfile.sh" ]; then
+								"$CURRENT_DIR/borgcron.sh" "$( get_full_path "$CONFIG_DIR/$configfile.sh" )"
+						else
+								echo "The backup settings file \"$configfile.sh\" could not be found." >&2
+						fi
+				done
+				;;
 esac
