e67f4dca67bbd4857f116c04bfc9d40daaf8c956
rugk <rugk@posteo.de>
2017-10-01 19:27:47 +0200

    remove accidentially added test file

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-i=1
-	read -rp "The backup failed. ($i. try) Do you want to try it again? [yN]: " retry
-	if [ "$retry" = "Y" ] || [ "$retry" = "y" ]; then
-		return 0 # (true)
-	fi
-	return 1 # (false)
